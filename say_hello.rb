61ecb2f1065c005f2eef263a8d733c8d26daf6e4
Elias Delgado <evdelgado99@gmail.com>
2020-08-06 22:31:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,11 @@
 # Build your say_hello method here
 
 def say_hello(name = "Ruby Programmer")
+  
   puts "Hello #{name}!"
+  
 end
+
+# Method Outputs "Hello Ruby Programmer" when a name is not provided. #
 
 puts say_hello
